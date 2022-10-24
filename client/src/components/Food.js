be5d4bc5cae4d00050b52b8d9faf1a114f9d42a1
Josh Levine <levine.joshuaadam@gmail.com>
2022-10-24 13:53:56 -0400

    delete food button functional

--- client/src/components/Food.js
+++ client/src/components/Food.js
@@ -1,7 +1,14 @@
+import axios from 'axios'
+
 const Food = (props) => {
+  //insert a delete button  here and then make it functional, then update button...add food should be a form that is NOT in this component
+
   return (
     <li>
-      {props.name} :{props.owner}'s {props.location}
+      {props.name} :{props.owner}'s {props.location}{' '}
+      <button id={props.id} onClick={props.deleteFood}>
+        X
+      </button>
     </li>
   )
 }
--- client/src/pages/HouseDetails.js
+++ client/src/pages/HouseDetails.js
@@ -8,20 +8,62 @@ const HouseDetails = (props) => {
   let { owner } = useParams()
 
   // const [houseDetails, setHouseDetails] = useState('')
-  const [foods, setFoods] = useState([])
+  // const [foods, setFoods] = useState([])
+  const [fridgeContents, setFridgeContents] = useState([])
 
   useEffect(() => {
-    const getFoods = async () => {
+    const getFridgeFoods = async () => {
       try {
         const response = await axios.get(`http://localhost:3001/foods`)
         console.log(response.data)
-        setFoods(response.data)
+        setFridgeContents(response.data)
       } catch (err) {
         console.log(err)
       }
     }
-    getFoods()
+    getFridgeFoods()
   }, [])
+
+  const deleteFood = async (event) => {
+    try {
+      //axios request
+      const response = await axios.delete(
+        `http://localhost:3001/foods/${event.target.id}`
+      )
+      //update state of fridgeContents
+      let updatedFridgeContents = []
+      fridgeContents.map((food) => {
+        if (food._id !== event.target.id) {
+          updatedFridgeContents.push(food)
+        }
+      })
+      setFridgeContents(updatedFridgeContents)
+    } catch (err) {
+      console.log(err)
+    }
+    // console.log(event.target.id)
+  }
+
+  const updateFood = async (event) => {
+    try {
+      //axios request
+      //HERE////
+      const response = await axios.update(
+        `http://localhost:3001/foods/${event.target.id}`
+      )
+      // //update state of fridgeContents
+      // let updatedFridgeContents = []
+      // fridgeContents.map((food) => {
+      //   if (food._id !== event.target.id) {
+      //     updatedFridgeContents.push(food)
+      //   }
+      // })
+      // setFridgeContents(updatedFridgeContents)
+    } catch (err) {
+      console.log(err)
+    }
+    // console.log(event.target.id)
+  }
 
   return (
     <div>
@@ -29,12 +71,15 @@ const HouseDetails = (props) => {
       <div id="joshs-fridge">
         <h3>Josh's Fridge</h3>
         <ul className="rearch-results">
-          {foods.map((food) => (
+          {fridgeContents.map((food) => (
             <Food
               key={food._id}
+              id={food._id}
               name={food.name}
               owner={food.owner}
               location={food.location}
+              deleteFood={deleteFood}
+              updateFood={updateFood}
             />
           ))}
         </ul>
